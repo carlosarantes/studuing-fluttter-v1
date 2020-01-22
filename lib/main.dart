1ab9a90d3762832addd5b9876309e27800c5c1b9
Carlos Augusto Arantes <carl.systemsbr@gmail.com>
2020-01-21 23:42:34 -0300

    refatorando app

--- lib/main.dart
+++ lib/main.dart
@@ -24,52 +24,24 @@ void main() => runApp( XerosoApp() );
       return Scaffold(
           appBar: AppBar(title : Text('Salvando tranferencias.', )),
           body: Column(children: <Widget>[
-
-                  Padding(padding: const EdgeInsets.all(16.0),
-                        child: TextField(
-                                  controller: _controladorCampoNumeroConta, 
-                                  style: TextStyle(
-                                    fontSize: 16.00
-                                  ),
-                                  decoration: InputDecoration(
-                                    labelText: 'Numero da conta',
-                                    hintText: '0000',
-                                  ),
-                                  keyboardType: TextInputType.number,
-                                ),
-                  ),
-
-                  Padding(padding: const EdgeInsets.all(16.0),
-                        child: TextField(
-                                  controller: _controladorCampoValor, 
-                                  style: TextStyle(
-                                    fontSize: 16.00
-                                  ),
-                                  decoration: InputDecoration(
-                                    icon: Icon(Icons.monetization_on),
-                                    labelText: 'Valor',
-                                    hintText: '0000',
-                                  ),
-                                  keyboardType: TextInputType.number,
-                                ),
-                  ),
+                  Editor( controlador:   _controladorCampoNumeroConta, rotulo : 'Numero da Conta', dica : '0000'),
+                  Editor( controlador :_controladorCampoValor, rotulo : 'Valor', dica : '0000', icone : Icons.monetization_on),
                   RaisedButton(
                     child: Text('Confirmar'), 
-                    onPressed: () {
-
-                      final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
-                      final double valor    = double.tryParse(_controladorCampoValor.text);
-
-                      if ( numeroConta != null &&  valor != null) {
-                        final transferenciaCriada = Transferencia(valor, numeroConta);
-                        debugPrint('$transferenciaCriada');
-                      }
-                    
-                    },
-                    
+                    onPressed: () => _criaTransferencia(),
                   )
           ],),
         );
+    }
+
+    void _criaTransferencia() {
+      final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+      final double valor    = double.tryParse(_controladorCampoValor.text);
+
+      if ( numeroConta != null &&  valor != null) {
+        final transferenciaCriada = Transferencia(valor, numeroConta);
+        debugPrint('$transferenciaCriada');
+      }
     }
   }
 
@@ -112,6 +84,40 @@ void main() => runApp( XerosoApp() );
     }
 
   }
+
+
+
+  class Editor extends StatelessWidget {
+
+    final TextEditingController controlador;
+    final String rotulo;
+    final String dica;
+    final IconData icone;
+
+    Editor({this.controlador, this.rotulo, this.dica, this.icone});
+
+    @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return Padding(padding: const EdgeInsets.all(16.0),
+                        child: TextField(
+                                  controller: controlador, 
+                                  style: TextStyle(
+                                    fontSize: 16.00
+                                  ),
+                                  decoration: InputDecoration(
+                                    icon:  icone != null ? Icon(icone) : null,
+                                    labelText: rotulo,
+                                    hintText: dica,
+                                  ),
+                                  keyboardType: TextInputType.number,
+                                ),
+                  );
+    }
+    
+  }
+
+
 
   class Transferencia {
 
